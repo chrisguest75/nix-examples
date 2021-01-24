7bc36b62e91b54323e6ebe32f5a26ff6c3f910b5
Chris Guest <chris.guest75@hotmail.com>
2021-01-24 22:07:16 +0000

    Add notes for installing krew.

--- 03_multi_package_docker/default.nix
+++ 03_multi_package_docker/default.nix
@@ -2,10 +2,11 @@
 
 pkgs.dockerTools.buildImage {
   name = "kuttl";
+  tag = "latest";
   config = {
     Cmd = [ "${pkgs.kubectl}/bin/kubectl" "kuttl" ];
   };
-
+  created = "now";
   contents = [
       pkgs.curl
       pkgs.bash
@@ -21,3 +22,16 @@ pkgs.dockerTools.buildImage {
 #pkgs.ca-certificates
 #pkgs.openssh-client
 #install kuttl
+
+
+# RUN curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/krew.tar.gz" && \ 
+
+# tar zxvf krew.tar.gz && \ 
+
+# KREW=./krew-"$(uname | tr '[:upper:]' '[:lower:]')_$(uname -m | sed -e 's/x86_64/amd64/' -e 's/arm.*$/arm/')" && \ 
+
+# "$KREW" install krew 
+
+# ENV PATH="/root/.krew/bin:$PATH" 
+
+# RUN kubectl krew install kuttl 
